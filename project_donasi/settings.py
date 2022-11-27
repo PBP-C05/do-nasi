e60e2099a0a9023e56b4a953fafe2070f881dd1d
frestytania <frestytania@gmail.com>
2022-11-27 15:55:53 +0700

    edit trusted origin

--- project_donasi/settings.py
+++ project_donasi/settings.py
@@ -152,3 +152,8 @@ for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add trusted origins
+CSRF_TRUSTED_ORIGINS = [
+    'https://do-nasi.up.railway.app'
+]
