da7d2b93e2bd737bc3622b06234cb941bd43e522
Monica Oktaviona <monicaoktaviona10@gmail.com>
2022-12-06 15:09:12 +0700

    fix login & get user authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login_flutter/', login, name='login_flutter'),
     path('signup_flutter/', register, name='signup_flutter'),
     path('logout_flutter/', logout, name='logout_flutter'),
-    path('user/', get_user, name='user')
+    path('get_user_json/', get_user, name='get_user_json'),
 ]
