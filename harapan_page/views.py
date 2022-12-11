4994205a0f961ec325ce081d556c911f7279260d
dreins <davynreinhard@gmail.com>
2022-12-11 18:07:36 +0700

    Harapan edited #5

--- harapan_page/views.py
+++ harapan_page/views.py
@@ -6,33 +6,7 @@ from landing_page.models import Pengguna
 from django.contrib import messages
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
-from django.utils import timezone
-
-
-
-# @login_required(login_url='/login/')
-# @csrf_exempt
-# def show_harapan(request):
-#   if request.user.is_authenticated:
-#        role = request.user.role
-#        if role == "Penyalur":
-#            pengguna = Pengguna.objects.get(username=request.user.username)
-#            data_harapan = HarapanDonatur.objects.filter(user=pengguna)
-#            context = {
-#                'roles': True,
-#               'data_harapan': data_harapan,
-#            }
-#            return render(request, "harapan_page.html", context)
-#        else:
-#            data_harapan = HarapanDonatur.objects.all()
-#            context = {
-#                'roles': False,
-#                'data_harapan': data_harapan,
-#            }
-#            return render(request, "harapan_page.html", context)
-#    else:
-#        return redirect('landing_page:login')
+from django.http import JsonResponse, HttpResponse
 
 def show_harapan(request):
     if request.user.is_authenticated:
@@ -51,7 +25,7 @@ def harapan_page(request):
     if request.method == 'POST':
         user = request.user
         text = request.POST.get('text')
-        HarapanDonatur.objects.all().delete()
+
         if  text is None:
             data = json.loads(request.body)
             text = data['text']
@@ -60,35 +34,6 @@ def harapan_page(request):
                 user=user, text=text, username=user, email=user.get_email())
             
             return JsonResponse({'message': 'Harapan Created!', 'error': False})
-    #return HttpResponseBadRequest("Bad request")
-    """if request.method == 'POST':
-        # retrieving data
-        pengguna = request.user
-        judul = request.POST.get('title')
-        isi = request.POST.get('body')
-        created_at = timezone.now()
-        text = request.POST.get('text')
-        
-        # making new instance and saving it
-        harapan = HarapanDonatur(
-            user = pengguna,
-            #title = judul,
-            created_at = created_at,
-            text = text
-        )
-        harapan.save()
-        
-        return JsonResponse({
-            "pk": harapan.pk,
-            "fields": {
-                "user":  harapan.user.username,
-                "username": harapan.user.username,
-                "email": harapan.user.email,
-                "created_at": harapan.created_at,
-                "text": harapan.text
-            }
-        })
-"""
 
 @csrf_exempt
 def show_harapan_json(request):
@@ -99,26 +44,6 @@ def show_harapan_json(request):
         serializers.serialize("json", data),
         content_type="application/json"
     )
-    """data = []
-    allHarapan = HarapanDonatur.objects.all()
-    print(allHarapan)
-
-    for harapan in allHarapan:
-        data.append({
-            "pk": harapan.pk,
-            "fields": {
-                "user" : { 
-                        "username" : harapan.user.username,
-                        "name"     : harapan.user.name,
-                        "role"     : harapan.user.role
-                        },
-                "username": harapan.user.username,
-                "email": harapan.user.email,
-                "created_at": harapan.created_at.strftime("%B %d, %Y at %H:%M %Z"),
-                "text": harapan.text
-            }
-        })
-    return JsonResponse(data, safe=False)"""
 
 
 @login_required(login_url='/login/')
